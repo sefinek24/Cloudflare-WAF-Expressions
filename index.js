9448373a18364dbf0b70f903ce6701a035ccca18
Sefinek <contact@sefinek.net>
2024-11-10 07:23:27 +0100

    Cron

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const pullAndUpdateWAFRules = async () => {
 		console.log(summary);
 
 		await executeWAFRuleUpdate();
-	} catch (error) {
-		console.error(`Git pull failed: ${error.message}`);
+	} catch (err) {
+		console.error('Git pull failed:', err.message);
 	}
 };
 
-new CronJob('0 10,15,20 * * *', pullAndUpdateWAFRules, null, true, 'UTC');
+new CronJob(process.env.RULES_UPDATE_SCHEDULE || '0 11,14,16,18,20 * * *', pullAndUpdateWAFRules, null, true, 'UTC'); // At minute 0 past hour 11, 14, 16, 18, and 20.
 (async () => pullAndUpdateWAFRules())();
 
 const restartApp = require('./jobs/reloadApp.js');
-new CronJob('5 10,15,20 * * *', restartApp, null, true, 'UTC');
+new CronJob(process.env.APP_RESTART_SCHEDULE || '0 15 */2 * *', restartApp, null, true, 'UTC'); // At 15:00 on every 2nd day-of-month.
