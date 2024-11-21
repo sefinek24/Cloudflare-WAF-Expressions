d9d510a79ab838ef9ece96c235b16d64f2a07474
Sefinek <sefinek5916@gmail.com>
2024-11-21 23:33:36 +0100

    Update headers

--- services/axios.js
+++ services/axios.js
@@ -4,7 +4,12 @@ module.exports = create({
 	baseURL: 'https://api.cloudflare.com/client/v4',
 	headers: {
 		'Authorization': `Bearer ${process.env.CF_API_TOKEN}`,
+		'Accept': 'application/json',
 		'Content-Type': 'application/json',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Accept-Language': 'pl;q=0.9',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive',
 	},
-	timeout: 45000,
+	timeout: 40000,
 });
