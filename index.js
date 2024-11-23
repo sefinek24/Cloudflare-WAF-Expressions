722373355e8ae74a1680e92a7fb4200eded98f0c
Sefinek <sefinek5916@gmail.com>
2024-11-23 12:19:34 +0100

    0 15 * * *

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ new CronJob(process.env.RULES_UPDATE_SCHEDULE || '0 11,14,16,18,20 * * *', pullA
 
 if (process.env.NODE_ENV === 'production') {
 	const restartApp = require('./jobs/reloadApp.js');
-	new CronJob(process.env.APP_RESTART_SCHEDULE || '0 15 */2 * *', restartApp, null, true, 'UTC'); // At 15:00 on every 2nd day-of-month.
+	new CronJob(process.env.APP_RESTART_SCHEDULE || '0 15 * * *', restartApp, null, true, 'UTC'); // At 15:00.
 }
